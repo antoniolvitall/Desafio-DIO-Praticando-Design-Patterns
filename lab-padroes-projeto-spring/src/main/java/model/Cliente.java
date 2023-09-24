d5fe311fb147b1601e4900247e83627273a2e4dd
Antonio Lindo <antoniolvitall@gmail.com>
2023-09-23 21:48:26 -0300

    Tentando corrigir bugs

---
+++ lab-padroes-projeto-spring/src/main/java/model/Cliente.java
@@ -0,0 +1,30 @@
+package model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne
+    private Endereco endereco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+}
