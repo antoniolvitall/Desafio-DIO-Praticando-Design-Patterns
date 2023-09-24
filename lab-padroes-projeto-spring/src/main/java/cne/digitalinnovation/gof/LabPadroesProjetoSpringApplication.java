d2c451fe32c377d6edc4fcc2d25b6f062486b975
Antonio Lindo <antoniolvitall@gmail.com>
2023-09-23 21:16:10 -0300

    Configuracoes iniciais

--- lab-padroes-projeto-spring/src/main/java/cne/digitalinnovation/gof/LabPadroesProjetoSpringApplication.java
+++ lab-padroes-projeto-spring/src/main/java/cne/digitalinnovation/gof/LabPadroesProjetoSpringApplication.java
@@ -2,7 +2,20 @@ package cne.digitalinnovation.gof;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * Projeto Spring Boot gerado via Spring Initializr
+ * Os seguintes modulos foram selecionados:
+ * -Spring Data JPA
+ * -Spring Web
+ * -H2 Database
+ * -OpenFeign
+ *
+ * @author Antonio Lindo
+ */
+
+@EnableFeignClients
 @SpringBootApplication
 public class LabPadroesProjetoSpringApplication {
 
